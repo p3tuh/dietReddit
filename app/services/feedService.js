4646d9aa5f467fd69982d65cf792506cfe328548
Peter Kim <chong.peter.kim@gmail.com>
2015-03-23 14:48:38 -0700

    Adding new subreddit working, but out of order & Duplicate bug

--- app/services/feedService.js
+++ app/services/feedService.js
@@ -3,13 +3,29 @@ angular.module('feedService', [])
 .service('feedService', function ($http, $q) {
 
   var getPosts = function(subName) {
-    var subUrl = 'http://api.reddit.com/hot?after=' + subName + '&jsonp=JSON_CALLBACK';
+    // var subUrl = 'http://api.reddit.com/hot?after=' + subName + '&jsonp=JSON_CALLBACK';
+    var subUrl = '';
+
+    if (subName) {
+      subUrl = 'http://api.reddit.com/r/' + subName + '/hot';
+    } else {
+      subUrl = 'http://api.reddit.com';
+    }
 
     console.log(subUrl);
 
-    return $http.jsonp(subUrl)
+    // return $http.jsonp(subUrl)
+    //   .then(function (resp) {
+    //     console.log('got data: ', resp.data);
+    //     return resp.data;
+    //   })
+    //   .catch(function (err) {
+    //     console.log(err);
+    //   });
+    
+    return $http.get(subUrl)
       .then(function (resp) {
-        console.log(resp.data);
+        console.log(resp.data.data);
         return resp.data;
       })
       .catch(function (err) {
